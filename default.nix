8e529680d8ddf0d5ab0d9c674a70050194ff74ed
James Hannah <git@james.jph.im>
2021-02-20 17:45:30 +0000

    fix default

--- default.nix
+++ default.nix
@@ -1,3 +1,8 @@
-let
-  sources = import ./nix/sources.nix
-in sources.nixpkgs
+{ sources ? import ./sources.nix }:     # import the sources
+with
+  { overlay = _: pkgs:
+      { niv = (import sources.niv {}).niv;    # use the sources :)
+      };
+  };
+import sources.nixpkgs                  # and use them again!
+  { overlays = [ overlay ] ; config = {}; }
